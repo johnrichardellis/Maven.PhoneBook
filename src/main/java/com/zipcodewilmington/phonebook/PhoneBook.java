78a2de538a4ad55ef6d836e0a7cc27a2f13648c0
Ellis John <ellisjohn@zipcodembpchuck.myfiosgateway.com>
2021-11-20 14:53:47 -0500

    lab

--- src/main/java/com/zipcodewilmington/phonebook/PhoneBook.java
+++ src/main/java/com/zipcodewilmington/phonebook/PhoneBook.java
@@ -13,41 +13,55 @@ import java.util.Map;
  */
 public class PhoneBook {
 
-    private final Map<String, List<String>> phonebook;
+    private List<String> phoneNumbers = new ArrayList<>();
+    private Map<String, List<String>> phonebook = new LinkedHashMap<>();
 
-    public PhoneBook(Map<String, List<String>> map) {
-        this.phonebook = null;
+
+    public PhoneBook(Map<String, List<String>> phonebookMap) {
+        this.phonebook = phonebookMap;
     }
 
     public PhoneBook() {
         this(null);
-    }
+    } // todo??
 
+    //add
     public void add(String name, String phoneNumber) {
+
+        phoneNumbers.add(phoneNumber);
+
+        phonebook.put(name, phoneNumbers);
     }
 
+    //addAll
     public void addAll(String name, String... phoneNumbers) {
     }
 
+    //remove
     public void remove(String name) {
     }
 
+    //hasEntry
     public Boolean hasEntry(String name) {
         return null;
     }
 
+    //lookup
     public List<String> lookup(String name) {
         return null;
     }
 
+    //reverseLookup
     public String reverseLookup(String phoneNumber)  {
         return null;
     }
 
+    //getAllContactNames
     public List<String> getAllContactNames() {
         return null;
     }
 
+    //getMap
     public Map<String, List<String>> getMap() {
         return null;
     }
