694d05be243f0d5d507633d726eda3b13b441023
Ellis John <ellisjohn@zipcodembpchuck.myfiosgateway.com>
2021-11-26 09:54:33 -0500

    lab

--- src/main/java/com/zipcodewilmington/phonebook/PhoneBook.java
+++ src/main/java/com/zipcodewilmington/phonebook/PhoneBook.java
@@ -1,11 +1,8 @@
 package com.zipcodewilmington.phonebook;
 
-import java.util.List;
-import java.util.ArrayList;
+import java.util.*;
 //import java.util.HashMap;
-import java.util.Iterator;
-import java.util.LinkedHashMap;
-import java.util.Map;
+
 
 /**
  * Created by leon on 1/23/18.
@@ -13,56 +10,93 @@ import java.util.Map;
  */
 public class PhoneBook {
 
-    private List<String> phoneNumbers = new ArrayList<>();
-    private Map<String, List<String>> phonebook = new LinkedHashMap<>();
+    // associated data type
+    private Map<String, List<String>> phoneBookMap = new LinkedHashMap<>();
+//    private Map<String, List<String>> phoneBook;   // could also declare this way
 
 
-    public PhoneBook(Map<String, List<String>> phonebookMap) {
-        this.phonebook = phonebookMap;
+    // constructor
+    public PhoneBook(Map<String, List<String>> phoneBook) {
+        this.phoneBookMap = phoneBook;
     }
 
+
+    // nullary constructor
     public PhoneBook() {
-        this(null);
-    } // todo??
+        phoneBookMap = new LinkedHashMap<>();
+    }
+
 
     //add
     public void add(String name, String phoneNumber) {
 
-        phoneNumbers.add(phoneNumber);
-
-        phonebook.put(name, phoneNumbers);
+        List<String> phoneNumberList = Arrays.asList(phoneNumber); // declaring new list phoneNumberList to hold the phoneNumber variable
+        phoneBookMap.put(name, phoneNumberList); // made a list, calling .put method on phoneBookMap list to add name and phoneNumbers
     }
 
     //addAll
     public void addAll(String name, String... phoneNumbers) {
+
+        List<String> phoneNumberList = Arrays.asList((phoneNumbers)); // creating new list to store phone numbers (2nd parameter)
+        phoneBookMap.put(name, phoneNumberList); // you add the above created list in as the 2nd parameter
+        // adding to the phoneBookMap list
     }
 
     //remove
     public void remove(String name) {
+
+        phoneBookMap.remove(name);
     }
 
     //hasEntry
     public Boolean hasEntry(String name) {
-        return null;
+
+        // checking to see if the phoneBookMap contains a certain name, and if so, return true.
+        if (phoneBookMap.containsKey(name)) {
+            return true;
+        } else {
+            // if the phoneBookMap doesnt contain name, then return false.
+            return false;
+        }
     }
 
     //lookup
     public List<String> lookup(String name) {
-        return null;
+        // this will retrieve the name in the given phoneBookMap
+        return phoneBookMap.get(name);
     }
 
     //reverseLookup
-    public String reverseLookup(String phoneNumber)  {
+    public String reverseLookup(String phoneNumber) {
+
+        //
+        for (Map.Entry<String, List<String>> entry : phoneBookMap.entrySet()) {
+            if (entry.getValue().contains(phoneNumber)) {
+                return entry.getKey();
+            }
+        }
         return null;
     }
 
     //getAllContactNames
     public List<String> getAllContactNames() {
-        return null;
+
+        // want to return a List<String> so you create one to use. allContactNames
+        List<String> allContactNames = new ArrayList<>();
+
+        // for each String type key in the phoneBookMap list .keySet
+        for (String key : phoneBookMap.keySet()) {
+
+            // add that key from phoneBookMap.keySet to the new allContactNames list
+            allContactNames.add(key);
+        }
+        //return this new list that now has all the key contact names in it
+        return allContactNames;
     }
 
     //getMap
     public Map<String, List<String>> getMap() {
-        return null;
+
+        return phoneBookMap;
     }
 }
